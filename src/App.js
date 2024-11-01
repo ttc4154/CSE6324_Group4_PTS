c88d7ff6e38299f3140b0d1d27eaef229d9d78f2
Toan Chu <164977414+ttc4154@users.noreply.github.com>
2024-11-01 11:10:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Services from './components/Services';
-import SearchBox from './components/SearchBox';
+// import SearchBox from './components/SearchBox';
 import servicesData from './servicesData';
 import './styles/Services.css'; 
 
 function App() {
     const [filteredServices, setFilteredServices] = useState(servicesData); // State for filtered services
 
-    const handleSearch = (searchTerm) => {
+    /*const handleSearch = (searchTerm) => {
         const lowercasedTerm = searchTerm.toLowerCase();
         const filtered = servicesData.filter(service =>
             service.name.toLowerCase().includes(lowercasedTerm) ||
             service.description.toLowerCase().includes(lowercasedTerm)
         );
         setFilteredServices(filtered);
-    };
+    };*/
 
     return (
         <Router>
